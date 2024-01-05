30ad9b6031071baa86760c5e5042b8eb55dc1992
kenzosashida090 <kenzo.sashida@clabsa.com.mx>
2024-01-05 11:36:59 -0600

    Cambios en el inicio

--- src/App.tsx
+++ src/App.tsx
@@ -7,42 +7,75 @@ const ExampleComponent = () => {
     <TypeAnimation
       sequence={[
         // Same substring at the start will only be typed out once, initially
-        "We produce the best markdown web app",
+        "We produce the best markdown web app !!!",
         1000, // wait 1s before replacing "Mice" with "Hamsters"
-        "We produce react  c;",
+        "We produce React !!!",
         1000,
+        "Save your notes here !!!",
+        1000,
+        "The best app to make your notes !!!",
+        ".......",
       ]}
       wrapper="span"
       speed={30}
-      style={{ fontSize: "2em", display: "inline-block" }}
+      style={{
+        fontSize: "2em",
+        display: "inline-block",
+        color: "white",
+        fontFamily: "Roboto",
+      }}
       repeat={Infinity}
     />
   );
 };
+
 export default function App() {
   return (
-    <div className="flex flex-row justify-center align-center items-center h-screen">
-      <div className="flex flex-col size-1/2 ml-24">
-        <p className="text-9xl font-bold">Welcome, Markdown React</p>
-        <div className="flex flex-row space-x-5 mt-5">
-          <Button className="rounded-full">Get Started</Button>
-          <Button className="rounded-full">Learn more</Button>
+    <>
+      <div className="flex flex-row justify-center align-center items-center h-screen bg-blue">
+        <div className="flex flex-col size-1/2 ml-20">
+          <p className="text-9xl font-bold">Welcome, Markdown React</p>
+          <div className="flex flex-row space-x-5 mt-5">
+            <Button className="rounded-full">Get Started</Button>
+            <Button
+              onClick={() => window.location.replace("/#about")}
+              className="rounded-full"
+            >
+              Learn more
+            </Button>
+          </div>
         </div>
+        <div className="max-w-2xl">
+          <div className="w-full h-11 rounded-t-lg bg-gray-200 flex justify-start items-center space-x-1.5 px-3">
+            <span className="w-3 h-3 rounded-full bg-red-400"></span>
+            <span className="w-3 h-3 rounded-full bg-yellow-400"></span>
+            <span className="w-3 h-3 rounded-full bg-green-400"></span>
+          </div>
+          <div className="bg-slate-900 border-t-0 w-96  h-48 ">
+            <ExampleComponent />
+          </div>
+        </div>
+        <div
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          aria-hidden="true"
+        ></div>
       </div>
-      <div className="max-w-2xl mx-auto">
-        <div className="w-full h-11 rounded-t-lg bg-gray-200 flex justify-start items-center space-x-1.5 px-3">
-          <span className="w-3 h-3 rounded-full bg-red-400"></span>
-          <span className="w-3 h-3 rounded-full bg-yellow-400"></span>
-          <span className="w-3 h-3 rounded-full bg-green-400"></span>
+      <section className="flex flex-col mt-40 ml-72 ">
+        <h1 id="about" className="text-8xl font-bold">
+          Why Markdown React ?
+        </h1>
+        <div className="flex w-3/5  mt-10 items-center justify-center">
+          <p className="items-center ml-7">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis quo
+            molestias non modi, alias architecto nobis harum porro beatae rem
+            iure libero in nemo similique nihil voluptas? Magnam, labore
+            numquam. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Odit quibusdam illum mollitia distinctio placeat blanditiis laborum
+            quod, porro est! Voluptatem suscipit repudiandae ex ut, placeat
+            doloremque cupiditate nesciunt nihil delectus!
+          </p>
         </div>
-        <div className="bg-gray-100 border-t-0 w-full  h-96 ">
-          <ExampleComponent />
-        </div>
-      </div>
-      <div
-        className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-        aria-hidden="true"
-      ></div>
-    </div>
+      </section>
+    </>
   );
 }
